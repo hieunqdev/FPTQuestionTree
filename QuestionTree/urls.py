c1b5b88a907134063606261f3fd9431c3e43e161
hieunqdev <hieunqdev@gmail.com>
2024-05-05 17:49:01 +0700

    QUESTION_TREE_QUEUE: feature add questions from the selected partner.

--- QuestionTree/urls.py
+++ QuestionTree/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import QuestionPartnerView, QuestionAdminView, QuestionDetailApiView
+from .views import QuestionPartnerView, QuestionAdminView, QuestionDetailApiView, QuestionDetailAdminApiView
 
 urlpatterns = [
     path('', QuestionPartnerView.as_view()),
     path('superuser', QuestionAdminView.as_view()),
-    path('questions/<int:id>', QuestionDetailApiView.as_view())
+    path('questions/<int:id>', QuestionDetailApiView.as_view()),
+    path('question', QuestionDetailAdminApiView.as_view())
 ]
