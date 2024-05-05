cb1ec51bca8534acfeb8ff111fd899a8973b409d
hieunqdev <hieunqdev@gmail.com>
2024-05-05 14:32:05 +0700

    QUESTION_TREE_PARTNER: feature add user and partner.

--- QuestionTree/urls.py
+++ QuestionTree/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import QuestionView
+from .views import QuestionPartnerView, QuestionAdminView, QuestionDetailApiView
 
 urlpatterns = [
-    path('', QuestionView.as_view()),
+    path('', QuestionPartnerView.as_view()),
+    path('superuser', QuestionAdminView.as_view()),
+    path('questions/<int:id>', QuestionDetailApiView.as_view())
 ]
